2ec201b7eb14fe792e93ad34021a629ee62551ba
theDirtyDeeds <ghost054rus@mail.ru>
2024-06-30 20:27:48 +0700

    Add files via upload

--- PracticalWork 8.1/PracticalWork 8.3/Program.cs
+++ PracticalWork 8.1/PracticalWork 8.3/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace PracticalWork_8._3
+﻿
+namespace PracticalWork_8._3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            HashSet<string> set = new HashSet<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Введите число в коллекцию");
+
+                string number = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(number)) break;
+
+                if (set.Contains(number))
+                {
+
+                    Console.WriteLine("Такое число уже есть");
+                }
+                else 
+                {
+                    set.Add(number);
+                    Console.WriteLine("Число добавлено в коллекцию");
+                }
+                
+            }
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
---
+++ PracticalWork 8.1/PracticalWork 8.4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Linq;
+
+namespace PracticalWork_8._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement person = new XElement("Person");
+
+            Console.WriteLine("Введите ФИО");
+
+            string name = Console.ReadLine();
+
+            person.Add(name);
+
+            XElement address  = new XElement("Address");
+
+            XElement street = new XElement("Street");
+
+            Console.WriteLine("Введите название улицы");
+
+            string streetName = Console.ReadLine();
+
+            street.Add(streetName);
+
+            XElement houseNumber = new XElement("HouseNumber");
+
+            Console.WriteLine("Введите номер дома");
+
+            string house = Console.ReadLine();
+
+            houseNumber.Add(house);
+
+            XElement flatNumber = new XElement("FlatNumber");
+
+            Console.WriteLine("Введите номер квартиры");
+
+            string apartmentNumber = Console.ReadLine();
+
+            flatNumber.Add(apartmentNumber);
+
+            address.Add(street, houseNumber, flatNumber);
+
+            XElement phones = new XElement("Phones");
+
+            XElement mobile = new XElement("MobilePhone");
+
+            Console.WriteLine("Введите номер мобильного");
+
+            string mobilePhone = Console.ReadLine();
+
+            mobile.Add(mobilePhone);
+
+            XElement flatPhone = new XElement("FlatPhone");
+
+            Console.WriteLine("Введите домашний номер");
+
+            string homePhone = Console.ReadLine();
+
+            flatPhone.Add(homePhone);
+
+            phones.Add(mobile, flatPhone);
+
+            person.Add(address, phones);
+
+            person.Save(@"\Notebook.xml");
+        }
+
+    }
+}
