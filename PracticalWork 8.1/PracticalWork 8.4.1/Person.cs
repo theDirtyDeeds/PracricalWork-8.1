ead00f9d4ff4034af774e8bef8784b86f668d4f3
theDirtyDeeds <ghost054rus@mail.ru>
2024-07-03 18:06:15 +0700

    Add files via upload

---
+++ PracticalWork 8.1/PracticalWork 8.4.1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork_8._4._1
+{
+    struct Person
+    {
+        public string Name { get; set; }
+
+        public string StreetName { get; set; }
+
+        public string HouseNumber { get; set; }
+
+        public string FlatNumber { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string FlatPhone { get; set; }
+    }
+    
+}
---
+++ PracticalWork 8.1/PracticalWork 8.4.1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Xml.Linq;
+
+namespace PracticalWork_8._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Filling(); 
+        }
+        static void Info(Person person) 
+        {
+            Console.WriteLine("Введите ФИО");
+
+            person.Name = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Введите название улицы");
+
+            person.StreetName = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Введите номер дома");
+
+            person.HouseNumber = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Введите номер квартиры");
+
+            person.FlatNumber = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Введите номер мобильного");
+
+            person.Mobile = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Введите домашний номер");
+
+            person.FlatPhone = Console.ReadLine() ?? "";
+        }
+        static void Filling() 
+        {
+            Person person = new Person();
+
+            Info(person);
+
+            XElement xPerson = new XElement("Person");
+
+            if(person.Name != null) 
+            { 
+
+            XAttribute name = new XAttribute("ФИО", person.Name);
+
+            xPerson.Add(name);
+            }
+
+
+            XElement address = new XElement("Address");
+
+            XElement street = new XElement("Street");
+
+            street.Add(person.StreetName);
+
+
+
+            XElement houseNumber = new XElement("HouseNumber");
+
+            houseNumber.Add(person.HouseNumber);
+
+            XElement flatNumber = new XElement("FlatNumber");
+
+            flatNumber.Add(person.FlatNumber);
+
+
+            address.Add(street, houseNumber, flatNumber);
+
+
+            XElement phones = new XElement("Phones");
+
+            XElement mobile = new XElement("MobilePhone");
+
+            mobile.Add(person.Mobile);
+
+
+            XElement flatPhone = new XElement("FlatPhone");
+
+            flatPhone.Add(person.FlatPhone);
+
+
+            phones.Add(mobile, flatPhone);
+
+
+            xPerson.Add(address, phones);
+
+
+            xPerson.Save(@"E:\Notebook2.xml");
+
+        }
+    }
+}
